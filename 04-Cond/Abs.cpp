8b30df47ca58e9d5c37029fb1257afa2c74a3ff8
Natalia Belen Damilano <natalia.b.damilano@accenture.com>
2020-06-03 22:20:43 -0300

    Se agregan las tres funciones modificando una de las librerías la cual era assert.h por cassert. Se quitan outputs e inputs innecesarios para la realización de este TP.

--- 04-Cond/Abs.cpp
+++ 04-Cond/Abs.cpp
@@ -1,34 +1,25 @@
 /* 04-Cond
-* Se desarrolla la función valor absoluto pidiéndole por teclado al usuario un entero para poder
-efectuar la ejecución de la función y así obtener su resultado.
+* Se desarrolla la función valor absoluto, para corroborar que cumpla las propiedades de la misma, se procede a hacer
+    pruebas con assert.
 * Natalia Belén Damilano
  * 31/5/2020
  */
 
 #include <iostream>
-#include <assert.h>     
+#include <cassert>     
 
 int Abs(int);
 
 int main()
 {
-    int num;
 
-    std::cout << "Este es un prototipo para determinar el resultado de una función valor absoluto |x|. \n"
-        "Ingrese un valor entero (Z) cualquiera para obtener el resultado final: ";
+    /* Este es un prototipo para determinar el resultado de una función valor absoluto |x|. */
 
-    std::cin >> num;
 
     assert(3 == Abs(-3));
     assert(0 == Abs(0));
     assert(3 == Abs(3));
 
-    num < 0 ? assert(-num == Abs(num)) : assert(num == Abs(num));
-
-    std::cout << "Resultado: |" + std::to_string(num) + "|=" + std::to_string(Abs(num));
-    std::cout << " \n";
-
-    system("pause");
 }
 
 int Abs(int x)
--- 04-Cond/F3.cpp
+++ 04-Cond/F3.cpp
@@ -1,24 +1,21 @@
 /* 04-Cond
-* Se desarrolla la función valor absoluto pidiéndole por teclado al usuario un entero para poder
-efectuar la ejecución de la función y así obtener su resultado.
+* Se desarrolla la función F3, la cual es una función por partes. Se procede a realizar pruebas con assert para verificar 
+    el correcto desarrollo de la función.
 * Natalia Belén Damilano
  * 31/5/2020
  */
 
 #include <iostream>
-#include <assert.h>     
+#include <cassert>     
 
 double F3(double);
 
 int main()
 {
-    std::cout << "Este es un prototipo para determinar el valor de la funcion F3 (funcion por partes) teniendo en cuenta el valor de la variable X. \n";
     
     assert(-4 == F3(-4));
     assert(-3 == F3(3));
     assert(4 == F3(4));
-
-    system("pause");
 
 }
 
