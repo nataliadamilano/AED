1678453a01658384ac4654f93eff2bf35a16e7a3
nataliadamilano <50343556+nataliadamilano@users.noreply.github.com>
2020-09-10 11:59:41 -0300

    Update Color.cpp

--- 04-Color/Color.cpp
+++ 04-Color/Color.cpp
@@ -1,7 +1,13 @@
+/* 04-Color
+ * Se pide realizar un tipo de dato Color a partir del modelo RGB, estableciendo un rango entre [0,255] para cada intensidad de los canales y se pide principalmente el
+ * desarrollar una función que permita mezclar dos colores en partes iguales.
+ * Natalia Belén Damilano
+ * 10/09/2020
+ */
+
 #include <cassert>
 #include <cmath>
 #include <cstring>
-#include <iostream>
 #include <stdio.h>
 
 using namespace std;
@@ -37,18 +43,16 @@ const Color negro = Restar(blanco, blanco);
 
 int main()
 {
-    cout << GetHtmlHex(rojo);
+  
 }
 
 Color Mezclar(Color color1, Color color2)
 {
     uint8_t avgRed = (color1.red + color2.red) / 2;
-    cout << unsigned(avgRed) << endl;
+   
     uint8_t avgGreen = (color1.green + color2.green) / 2;
-    cout << unsigned(avgGreen) << endl;
+    
     uint8_t avgBlue = (color1.blue + color2.blue) / 2;
-    cout << unsigned(avgBlue);
-
 
     return { avgRed, avgGreen, avgBlue };
 }
@@ -56,11 +60,10 @@ Color Mezclar(Color color1, Color color2)
 Color MezclarProporciones(Color color1, unsigned partesColor1, Color color2, unsigned partesColor2)
 {
     uint8_t avgRed = (color1.red + color2.red) / (partesColor1 + partesColor2);
-    cout << unsigned(avgRed) << endl;
+    
     uint8_t avgGreen = (color1.green + color2.green) / (partesColor1 + partesColor2);
-    cout << unsigned(avgGreen) << endl;
+
     uint8_t avgBlue = (color1.blue + color2.blue) / (partesColor1 + partesColor2);
-    cout << unsigned(avgBlue);
 
     return { avgRed, avgGreen, avgBlue };
 }
@@ -69,7 +72,9 @@ Color MezclarProporciones(Color color1, unsigned partesColor1, Color color2, uns
 Color Sumar(Color color1, Color color2)
 {
     uint8_t sumRed = color1.red + color2.red > MAX ? MAX : color1.red + color2.red;
+    
     uint8_t sumGreen = color1.green + color2.green > MAX ? MAX : color1.green + color2.green;
+    
     uint8_t sumBlue = color1.blue + color2.blue > MAX ? MAX : color1.blue + color2.blue;
 
     return { sumRed, sumGreen, sumBlue };
@@ -78,7 +83,9 @@ Color Sumar(Color color1, Color color2)
 Color Restar(Color color1, Color color2)
 {
     uint8_t sumRed = color1.red - color2.red < MIN ? MIN : color1.red - color2.red;
+    
     uint8_t sumGreen = color1.green - color2.green < MIN ? MIN : color1.green - color2.green;
+    
     uint8_t sumBlue = color1.blue - color2.blue < MIN ? MIN : color1.blue - color2.blue;
 
     return { sumRed, sumGreen, sumBlue };
